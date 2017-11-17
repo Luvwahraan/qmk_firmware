759dfcd89fb67920b620fd8db600a27b7d52fb0d
Luvwahraan <luvwahraan@pikaed.org>
2017-11-17 16:47:54 +0100

    add U8 (bin to ushort) inline function

--- keyboards/ergodone/keymaps/bepo_luvw/utils.h
+++ keyboards/ergodone/keymaps/bepo_luvw/utils.h
@@ -3,7 +3,11 @@
 
 #define U32(layer)  (1UL << layer)
 
-enum next_key_down_up{
+
+uint8_t U8(uint8_t);
+
+enum next_key_down_up
+{
     NK_DOWN_UP,
     NK_DOWN,
     NK_UP // a bit of a hack, this works as long as NK_UP < KC_A
